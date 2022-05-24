ee4ccfc618dff0b13949806ba942bf3fdaff6bda
pablo-marrero <pablomarrero241096@gmail.com>
2022-05-24 15:05:25 -0300

    cart in process

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./ProductCard.css";
-export default function ProductCard({ name, price, image, rating }) {
+export default function ProductCard({ name, price, image, rating, handleSaveCart }) {
   return (
     <article className="productCard-container">
+      <button onClick={handleSaveCart}>agregar carrito de gatitos</button>
       <img src={`${image}`} />
       <div className="productCard-info">
         <p>{name}</p>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,23 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useState,useEffect } from "react";
 import ProductCard from "../../components/ProductCard/ProductCard.jsx";
 import { useStore } from "../../context/store.js";
 import { fetchProducts } from "../../redux/actions/actions.js";
 import "./Home.css";
 
 export default function Home() {
+
   const [state, dispatch] = useStore();
+  let initialCart = JSON.parse(localStorage.getItem("myCart")) || []
+  const [cart, setCart] = useState(initialCart)
+
+
+  const handleSaveCart = ()=>{
+  
+    alert("funco")
+  }
+
+  useEffect(() => {
+    localStorage.setItem("myCart",JSON.stringify(cart))
+  }, [cart])
+  
+
+
   console.log(state.products);
   return (
     <section className="setion-products">
       {state.products &&
         state.products.map((product) => {
           return (
+
             <ProductCard
               name={product.name}
               key={product.id}
               price={product.price}
               image={product.image}
               rating={product.rating}
+              handleSaveCart={handleSaveCart}
             />
           );
         })}
