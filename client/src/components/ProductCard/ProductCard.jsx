f50bc91e80ec7ab1719cc303ea645e5dec3f024f
tathapachon <85940447+tathapachon@users.noreply.github.com>
2022-05-24 17:30:13 -0500

    Card Css

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./ProductCard.css";
+import {Link} from "react-router-dom"
 export default function ProductCard({ name, price, image, rating }) {
   return (
-    <article className="productCard-container">
-      <img src={`${image}`} />
+    <Link to={"/home/:id"}>
+     <article className="card card-style">
+      <div className="img-container">
+          <img src={`${image}`} alt={`${name}`} />
+          <span className="price">{`$${price}`}</span>
+      </div>
+      
       <div className="productCard-info">
-        <p>{name}</p>
-        <p>{price}</p>
-        <p>{rating}</p>
+        <span>{name}</span>        
+        <span>{rating}</span>
       </div>
     </article>
+    </Link>
+   
   );
 }
