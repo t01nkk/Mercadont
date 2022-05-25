67a27f90507deb14dc009922d92e26a93ae5f8be
RodriMR <rodrimr2008@gmail.com>
2022-05-25 03:25:07 -0300

    Update visual

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,31 +1,38 @@
 import React from "react";
 import "./ProductCard.css";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import shoppingCart from "../../media/shoppingCart.png";
 
-
-export default function ProductCard({ name, price, image, rating, handleSaveCart }) {
-
-
-
+export default function ProductCard({
+  name,
+  price,
+  image,
+  rating,
+  handleSaveCart,
+}) {
   return (
     // <div></div>
-    
+
     <div>
-        <button onClick={()=>handleSaveCart(name, price)}>agregar carrito</button>
+      <button
+        onClick={() => handleSaveCart(name, price)}
+        className="shoppingCart-btn"
+      >
+        <img src={shoppingCart} alt="" />
+      </button>
       <Link to={"/home/:id"}>
-       <article className="card card-style">
-        <div className="img-container">
+        <article className="card card-style">
+          <div className="img-container">
             <img src={`${image}`} alt={`${name}`} />
             <span className="price">{`$${price}`}</span>
-        </div>
-        
-        <div className="productCard-info">
-          <span>{name}</span>        
-          <span>{rating}</span>
-        </div>
-      </article>
+          </div>
+
+          <div className="productCard-info">
+            <span>{name}</span>
+            <span>{rating}</span>
+          </div>
+        </article>
       </Link>
     </div>
-   
   );
 }
--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-
+import "./SearchBar.css";
+import icon from "../../media/search.png";
 export default function SearchBar() {
   return (
     <div>
-      <form>
-        <input type="text" />
-        <button type="submit">Search</button>
+      {/* <form className="searchBar-container">
+        <input type="search" className="search-box" placeholder="Looking for something?"/>
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form> */}
+
+      <form onsubmit="event.preventDefault();" role="search" className="searchBar-container">
+        <input
+          id="search"
+          type="text"
+          placeholder="Search..."
+          autofocus
+          required
+        />
+        <button type="submit">
+          <img src={icon} alt="" />
+        </button>
       </form>
     </div>
   );
