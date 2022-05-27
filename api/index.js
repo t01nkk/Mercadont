b8f7eb2e33ac1617e6a68b26733dde1993289328
Ignacio W <williamsignacio89@gmail.com>
2022-05-27 12:56:35 -0300

    New tables/models/relations

--- api/index.js
+++ api/index.js
@@ -33,39 +33,9 @@ const { Product, Category } = require("./src/db");
 const { getProducts } = require("./src/middlewares/middlewares")
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(3001, async () => {
-
     await getProducts();
-    /*
-        const cat = await Category.create({name:"Testing"})
-        const test1 = await Product.create({
-            name: "Test1",
-            price: 100,
-            description: "Test Product",
-            rating: 5,
-            image: null,
-            stock: 1,
-        })
-
-        const test2 = await Product.create({
-            name: "Test2",
-            price: 10,
-            description: "Test Product 2",
-            rating: 1,
-            image: null,
-            stock: 1,
-        })
-
-        const test3 = await Product.create({
-            name: "Test3",
-            price: 10,
-            description: "Test Product 3",
-            rating: 5,
-            image: null,
-            stock: 1,
-        })
-*/
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
---
+++ api/src/models/questionsAnswers.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+module.exports = (sequelize) => {
+    sequelize.define('qa', {
+        question: {
+            type: DataTypes.TEXT,
+        },
+        answer:{
+            type: DataTypes.TEXT,
+        },
+        resolved: {
+            type: DataTypes.BOOLEAN,
+            default: false,
+        }
+    })
+};
