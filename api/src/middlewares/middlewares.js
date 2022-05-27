667947b294e617d8b6741793ef5c548785a9da0c
limperatri <lautaro.d.gutierrez@gmail.com>
2022-05-27 01:58:44 -0300

    Middleware actualizado+

--- api/src/middlewares/middlewares.js
+++ api/src/middlewares/middlewares.js
@@ -26,40 +26,24 @@ function initialize(passport, getUserByEmail, getUserById) {
     passport.deserializeUser((id, done) => done(null, getUserById(id)))
 }
 
-<<<<<<< HEAD
-async function getProducts() {
-    const findCreated = await Product.findAll({ where: { created: true } })
-    let count = await Product.count();
-    if (findCreated.length === count) {
-        for (let i = 0; i < productos.length; i++) {
-            const newProduct = await Product.create({
-                name: productos[i].name,
-                price: productos[i].price,
-                description: productos[i].description,
-                rating: productos[i].rating,
-                image: productos[i].image,
-                stock: productos[i].stock,
-                db: true
-            })
-=======
-function validateInputUser(name, lastname, email, password){
+function validateInputUser(name, lastname, email, password) {
     let errors = [];
     if (!/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) errors.push("Email address is not valid");
-    if(!name || name.length > 30) errors.push("Name is not valid");
-    if(!lastname || lastname.length > 30) errors.push("Last name is not valid");
-    if(!/(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/.test(password)) errors.push("Password must have 1 lowercase letter, 1 uppercase letter, 1 number, and be at least 8 characters long");
+    if (!name || name.length > 30) errors.push("Name is not valid");
+    if (!lastname || lastname.length > 30) errors.push("Last name is not valid");
+    if (!/(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/.test(password)) errors.push("Password must have 1 lowercase letter, 1 uppercase letter, 1 number, and be at least 8 characters long");
     //VALIDATE PAYMENT AND ADDRESS??????????? 
     return errors;
 }
 
-function validateInputProduct(name,price,description,image,stock,categories){
+function validateInputProduct(name, price, description, image, stock, categories) {
     let errors = [];
-    if(!name || name.length > 60) errors.push("Name is not valid");
-    if(!price || price < 1) errors.push("Price is not valid");
-    if(!description) errors.push("Description is not valid");
-    if(!image) errors.push("Please, add at least one image of the product");
-    if(!stock || stock < 1) errors.push("Stock is not valid");
-    if(!categories.length) errors.push("Please, add at least one category that the product belongs to");
+    if (!name || name.length > 60) errors.push("Name is not valid");
+    if (!price || price < 1) errors.push("Price is not valid");
+    if (!description) errors.push("Description is not valid");
+    if (!image) errors.push("Please, add at least one image of the product");
+    if (!stock || stock < 1) errors.push("Stock is not valid");
+    if (!categories.length) errors.push("Please, add at least one category that the product belongs to");
     return errors;
 }
 
@@ -79,7 +63,21 @@ function validateInputProduct(name,price,description,image,stock,categories){
 //         return done(err);
 //       })
 //     }));
->>>>>>> refs/remotes/origin/Dev-Branch
+
+async function getProducts() {
+    const findCreated = await Product.findAll({ where: { created: true } })
+    let count = await Product.count();
+    if (findCreated.length === count) {
+        for (let i = 0; i < productos.length; i++) {
+            const newProduct = await Product.create({
+                name: productos[i].name,
+                price: productos[i].price,
+                description: productos[i].description,
+                rating: productos[i].rating,
+                image: productos[i].image,
+                stock: productos[i].stock,
+                db: true
+            })
 
             for (let j = 0; j < productos[i].categories.length; j++) {
 
@@ -110,10 +108,7 @@ function validateInputProduct(name,price,description,image,stock,categories){
 }
 module.exports = {
     initialize,
-<<<<<<< HEAD
-    getProducts
-=======
+    getProducts,
     validateInputUser,
     validateInputProduct
->>>>>>> refs/remotes/origin/Dev-Branch
 }
