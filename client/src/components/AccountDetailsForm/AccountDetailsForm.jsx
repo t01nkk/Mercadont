ef7b8cac5763f458ad7668f7302982b593531b1d
RodriMR <rodrimr2008@gmail.com>
2022-05-28 19:46:59 -0300

    creo que funciona, les pido perdon si no uwu

--- client/src/components/AccountDetailsForm/AccountDetailsForm.jsx
+++ client/src/components/AccountDetailsForm/AccountDetailsForm.jsx
@@ -1,11 +1,16 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useStore } from "../../context/store";
 
 export default function AccountDetailsForm() {
   const [user, setUser] = useState();
-
+  const [state, dispatch] = useStore();
   const idUser = state.user;
-  const fetchUser = (id) => {};
-  useEffect(() => {}, []);
+  const fetchUser = async (idUser) => {
+    const userDB = await axios.get();
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return <div>AccountDetails FORM</div>;
 }
