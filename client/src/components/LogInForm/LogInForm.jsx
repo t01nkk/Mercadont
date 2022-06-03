648783a87b0940ee9e7485b3a0d2019140ca6a43
limperatri <lautaro.d.gutierrez@gmail.com>
2022-06-03 01:50:20 -0300

    cosas

--- client/src/components/LogInForm/LogInForm.jsx
+++ client/src/components/LogInForm/LogInForm.jsx
@@ -38,14 +38,7 @@ export default function LogInForm() {
       //   url: `${process.env.REACT_APP_DOMAIN}/user/login`,
       // });
       // console.log(user);
-      const user = await axios.post(`${process.env.REACT_APP_DOMAIN}/user/login`,
-        {
-          data: {
-            email: data.email,
-            password: data.password,
-          }
-        }
-      );
+      const user = await axios.post(`${process.env.REACT_APP_DOMAIN}/user/login`, data);
       console.log("user: ", user);
       if (user.data.passport.user) {
         localStorage.setItem("myUser", JSON.stringify(user.data.passport.user));
