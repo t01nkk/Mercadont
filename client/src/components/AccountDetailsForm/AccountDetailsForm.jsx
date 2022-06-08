250b6bed1e3ce3bba98f0328f71d66aeb2609025
RodriMR <rodrimr2008@gmail.com>
2022-06-08 11:49:47 -0300

    Navbar logged responsive y hermosa como tu señora

--- client/src/components/AccountDetailsForm/AccountDetailsForm.jsx
+++ client/src/components/AccountDetailsForm/AccountDetailsForm.jsx
@@ -1,15 +1,15 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useStore } from "../../context/store";
 import { ToastContainer, toast } from "react-toastify";
 import { useAuth } from "../../context/authContext";
 import { useTranslation } from "react-i18next";
 export default function AccountDetailsForm() {
-  const { t } = useTranslation()
-  const history = useHistory()
+  const { t } = useTranslation();
+  const history = useHistory();
 
- // 'User information updated successfully'
+  // 'User information updated successfully'
   const alertUserUpdated = (msg) => {
     toast.info(msg, {
       position: "bottom-center",
@@ -19,9 +19,9 @@ export default function AccountDetailsForm() {
       pauseOnHover: false,
       draggable: false,
       progress: undefined,
-      theme: "dark"
-    })
-  }
+      theme: "dark",
+    });
+  };
 
   const { resetPassword } = useAuth();
   const [state, dispatch] = useStore();
@@ -36,10 +36,8 @@ export default function AccountDetailsForm() {
     postalCode: "",
     password: "",
     image: "",
-
-
   });
-  let id = localStorage.getItem("myUser")
+  let id = localStorage.getItem("myUser");
 
   const fetchUser = async () => {
     // console.log(state.user)
@@ -48,11 +46,11 @@ export default function AccountDetailsForm() {
       const userDB = await axios.get(
         `${process.env.REACT_APP_DOMAIN}/user/details/${miStorage}`
       );
-      console.log("user", userDB)
+      console.log("user", userDB);
       setUser(userDB.data);
     } catch (err) {
       // console.log(err);
-      return err
+      return err;
     }
   };
   useEffect(() => {
@@ -61,142 +59,151 @@ export default function AccountDetailsForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { email, name, lastname, image, password,country,province, city, street, postalCode } = user;
+    const {
+      email,
+      name,
+      lastname,
+      image,
+      password,
+      country,
+      province,
+      city,
+      street,
+      postalCode,
+    } = user;
     // console.log(id)
     try {
-      await axios.put(`${process.env.REACT_APP_DOMAIN}/user/details/${state.user}`, {
-        email,
-        name,
-        lastname,
-        image,
-        password,
-        country,
-        province,
-        city,
-        street,
-        postalCode
-      })
-      alertUserUpdated(t("accountDetailsForm.toastInfo"))
+      await axios.put(
+        `${process.env.REACT_APP_DOMAIN}/user/details/${state.user}`,
+        {
+          email,
+          name,
+          lastname,
+          image,
+          password,
+          country,
+          province,
+          city,
+          street,
+          postalCode,
+        }
+      );
+      alertUserUpdated(t("accountDetailsForm.toastInfo"));
       setTimeout(() => {
-        history.push('/accountDetails')
+        history.push("/accountDetails");
       }, 4000);
     } catch (err) {
       // console.log(err);
-      return err
+      return err;
     }
   };
   const handleChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
 
-
-
   const handleResetPassword = async (e) => {
     e.preventDefault();
     // console.log("handleResetPassword USER:",user)
-    const answer = window.confirm(t("accountDetailsForm.askPasswordChange"))
-    if(answer){
+    const answer = window.confirm(t("accountDetailsForm.askPasswordChange"));
+    if (answer) {
       try {
-        await resetPassword(user.email)
-        alert(t("accountDetailsForm.confirmPasswordChange"))
-      }
-      catch (err) {
-        alert(err.message)
+        await resetPassword(user.email);
+        alert(t("accountDetailsForm.confirmPasswordChange"));
+      } catch (err) {
+        alert(err.message);
       }
     }
-  }
+  };
 
   return (
     <div>
       <h2>{t("accountDetailsForm.updateInfo")}</h2>
       <form onSubmit={handleSubmit}>
-      <div className="divInputUser">
-      <label className="title">{t("accountDetailsForm.email")}: </label>
-        <input
-          type="email"
-          name="email"
-          value={user.email}        
-          onChange={handleChange}
-        />
+        <div className="divInputUser">
+          <label className="title">{t("accountDetailsForm.email")}: </label>
+          <input
+            type="email"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
+          />
         </div>
-          <div className="divInputUser">
+        <div className="divInputUser">
           <p className="title">{t("accountDetailsForm.name")}: </p>
-        <input
-          type="text"
-          name="name"         
-          value={user.name}
-          onChange={handleChange}
-        />
-      </div>
-        
-      <div className="divInputUser">
-        <p className="title">{t("accountDetailsForm.lastname")}: </p>
-        <input
-          type="text"
-          name="lastname"
-          value={user.lastname}
-          onChange={handleChange}
-        />
-      </div>
-        
-       <div className="divInputUser">
-         <p className="title">{t("accountDetailsForm.address")}</p>
-        <input
-          type="text"
-          name="city"
-          placeholder={t("accountDetailsForm.city")}
-          value={user.address?.city}
-          onChange={handleChange}
-        />         
-         </div> 
-         <div className="divInputUser">
-        
-        <input
-          type="text"
-          name="country"
-          placeholder={t("accountDetailsForm.country")}
-          value={user.address?.country}
-          onChange={handleChange}
-        />         
-         </div> 
-         <div className="divInputUser">
-  
-        <input
-          type="text"
-          name="postalCode"
-          placeholder={t("accountDetailsForm.postalCode")}
-          value={user.address?.postalCode}
-          onChange={handleChange}
-        />         
-         </div>    
-         <div className="divInputUser">
-         
-        <input
-          type="text"
-          name="province"
-          placeholder={t("accountDetailsForm.province")}
-          value={user.address?.province}
-          onChange={handleChange}
-        />         
-         </div> 
-         <div className="divInputUser">
-       
-        <input
-          type="text"
-          name="street"
-          placeholder={t("accountDetailsForm.street")}
-          value={user.address?.street}
-          onChange={handleChange}
-        />         
-         </div>  
+          <input
+            type="text"
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+          />
+        </div>
 
-      <div className="">
-        <p className="title">{t("accountDetailsForm.password")}: </p>
-        <button onClick={(e) => handleResetPassword(e)}>{t("accountDetailsForm.changePassword")}</button>       
-      </div>
+        <div className="divInputUser">
+          <p className="title">{t("accountDetailsForm.lastname")}: </p>
+          <input
+            type="text"
+            name="lastname"
+            value={user.lastname}
+            onChange={handleChange}
+          />
+        </div>
 
-      <div className="divInputUser">
-        <p className="title">{t("accountDetailsForm.image")}: </p>
+        <div className="divInputUser">
+          <p className="title">{t("accountDetailsForm.address")}</p>
+          <input
+            type="text"
+            name="city"
+            placeholder={t("accountDetailsForm.city")}
+            value={user.address?.city}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="divInputUser">
+          <input
+            type="text"
+            name="country"
+            placeholder={t("accountDetailsForm.country")}
+            value={user.address?.country}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="divInputUser">
+          <input
+            type="text"
+            name="postalCode"
+            placeholder={t("accountDetailsForm.postalCode")}
+            value={user.address?.postalCode}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="divInputUser">
+          <input
+            type="text"
+            name="province"
+            placeholder={t("accountDetailsForm.province")}
+            value={user.address?.province}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="divInputUser">
+          <input
+            type="text"
+            name="street"
+            placeholder={t("accountDetailsForm.street")}
+            value={user.address?.street}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="">
+          <p className="title">{t("accountDetailsForm.password")}: </p>
+          <button onClick={(e) => handleResetPassword(e)}>
+            {t("accountDetailsForm.changePassword")}
+          </button>
+        </div>
+
+        <div className="divInputUser">
+          <p className="title">{t("accountDetailsForm.image")}: </p>
           <input
             type="text"
             name="image"
@@ -208,9 +215,8 @@ export default function AccountDetailsForm() {
         <div className="btn-login">
           <input type="submit" name="Update info" className="input-submit" />
         </div>
-
       </form>
-       <ToastContainer />
+      <ToastContainer />
     </div>
-  )
+  );
 }
