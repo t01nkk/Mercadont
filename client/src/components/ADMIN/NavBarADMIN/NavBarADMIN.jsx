deddf256dfa29299dc7f890a1d75bf7e669dba92
RodriMR <rodrimr2008@gmail.com>
2022-06-11 18:37:49 -0300

    admin session check, sino te patea al login

--- client/src/components/ADMIN/NavBarADMIN/NavBarADMIN.jsx
+++ client/src/components/ADMIN/NavBarADMIN/NavBarADMIN.jsx
@@ -4,14 +4,39 @@ import logo from "../../../media/header_logo.png";
 import LoggedAdmin from "./LoggedAdmin/LoggedAdmin";
 import UnloggedAdmin from "./UnloggedAdmin/UnloggedAdmin";
 import { useStore } from "../../../context/store";
-import {
-  checkSessionADMIN,
-  fetchCategories,
-} from "../../../redux/actions/actions";
+import { fetchCategories } from "../../../redux/actions/actions";
+import { useHistory } from "react-router-dom";
+import { ADMIN_SESSION } from "../../../redux/actions/actionTypes";
 
 export default function NavBarADMIN() {
-
   const [state, dispatch] = useStore();
+  const history = useHistory();
+  const checkSessionADMIN = async (dispatch) => {
+    let loggedAdmin = false;
+    let loggedAdminInfo = JSON.parse(localStorage.getItem("myAdmin"));
+    if (loggedAdminInfo !== null) {
+      if (loggedAdminInfo?.data.isAdmin) {
+        loggedAdmin = true;
+        await dispatch({
+          type: ADMIN_SESSION,
+          payload: {
+            admin: loggedAdminInfo,
+            sessionAdmin: loggedAdmin,
+          },
+        });
+      }
+    } else {
+      await dispatch({
+        type: ADMIN_SESSION,
+        payload: {
+          sessionAdmin: loggedAdmin,
+        },
+      });
+      history.push(
+        "/CC7E389029C4B7768A0C89DC75F304059EF9ECBA68FF02FD4BFB7FE740721F4F/admin/login"
+      );
+    }
+  };
   useEffect(() => {
     fetchCategories(dispatch);
   }, []);
