c08100e406904f26cd11169f99c0f1572170783d
pablo-marrero <pablomarrero241096@gmail.com>
2022-06-11 01:52:34 -0300

    Sigo modificando

--- client/src/pages/ADMIN/QaS/QaSIndividual.jsx
+++ client/src/pages/ADMIN/QaS/QaSIndividual.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useState }from 'react'
-import { useHistory } from 'react-router-dom'
+import { useHistory,Redirect } from 'react-router-dom'
 
 export const QaSIndividual = ({id,idProduct,createdAt,name,userId,question,answer}) => {
   const [textArea, setTextArea] = useState("")
@@ -10,10 +10,15 @@ export const QaSIndividual = ({id,idProduct,createdAt,name,userId,question,answe
   const sendQaS = async()=>{
     console.log(textArea)
     try {
-      await axios.put(`${process.env.REACT_APP_DOMAIN}/admin/${id}/answer`,{
+      if(!textArea){alert("por favor complete el campo")}
+      let sendAnswerAdmin = await axios.put(`${process.env.REACT_APP_DOMAIN}/admin/${id}/answer`,{
         answer:textArea,
         idProduct
       })
+      if(sendAnswerAdmin){
+        window.location.reload()
+        // Redirect("/CC7E389029C4B7768A0C89DC75F304059EF9ECBA68FF02FD4BFB7FE740721F4F/admin/QaS")
+      }
     } catch (error) {
       console.log(error)
     }
@@ -40,7 +45,7 @@ export const QaSIndividual = ({id,idProduct,createdAt,name,userId,question,answe
         
         {changeQaS?<div>
           <button onClick={()=>setChangeQaS(false)}>X</button>
-          <textarea name="" id="" cols="20" rows="5" value={textArea} onChange={handleChange}></textarea>
+          <textarea name="" id="" cols="20" rows="5" value={textArea} required onChange={handleChange}></textarea>
         <button onClick={()=>sendQaS()}>Send answer</button>
         </div>:null}
         
