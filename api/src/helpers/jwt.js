157ad1d5a3cf9fee2f31b6995de0337c813dbd6c
t01nk <lautaro.d.gutierrez@gmail.com>
2024-04-27 22:28:31 -0300

    added prittier config file - added auth login route

--- api/src/helpers/jwt.js
+++ api/src/helpers/jwt.js
@@ -1,15 +1,20 @@
 const jwt = require('jsonwebtoken');
-
 const generateAccessToken = (id = '') => {
     return new Promise((resolve, reject) => {
-        const payload = { id }
-        jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { //payload MUST be an object or it will be treated as a string (treats it as option expiresIn)
-            expiresIn: 60 * 60
-        }, (err, token) => {
-            if (err) { reject({ message: err.message }) }
-            else resolve(token);
-        })
-    })
-}
+        const payload = { id };
+        jwt.sign(
+            payload,
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn: '60s',
+            },
+            (err, token) => {
+                if (err) {
+                    reject({ message: err.message });
+                } else resolve(token);
+            }
+        );
+    });
+};
 
 module.exports = { generateAccessToken };
--- api/src/routes/authroutes.js
+++ api/src/routes/authroutes.js
@@ -1,54 +1,56 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const router = Router();
-const { User } = require("../db");
-const bcrypt = require("bcrypt");
-const sequelize = require('sequelize')
-const { generateAccessToken } = require("../helpers/jwt");
-// const { groupPurchaseOrders } = require("../middlewares/middlewares");
+const { User } = require('../db');
+const bcrypt = require('bcrypt');
+const { generateAccessToken } = require('../helpers/jwt');
 
-
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
     const { name, lastname, password, email, cart } = req.body;
     try {
         const userExist = await User.findOne({ where: { email: email } });
-        if (userExist) throw new Error("Email in use")
-        const salt = 10;
-        const passwordHash = bcrypt.hashSync(password[0], salt);
+        if (userExist) res.status(401).send({ message: 'User already exists' });
+
+        const salt = bcrypt.genSaltSync(10);
+        const passwordHash = bcrypt.hashSync(password, salt);
 
         const createdUser = await User.create({
             email: email,
             password: [passwordHash],
             name: name,
             lastname: lastname,
-            cart: cart?.length ? cart : []
+            cart: cart?.length ? cart : [],
         });
 
         const token = await generateAccessToken(createdUser.id);
 
-        return res.send({ message: "User Registered", token, createdUser });
-
+        return res.status(201).send({ message: 'User Registered', token });
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
 });
 
-router.post("/login", async (req, res, next) => {
-    // const { email, password } = req.body;
-    const { name, email, image, id } = req.body;
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
     try {
-        const userExist = await User.findOrCreate({
-            where: { id: id }, defaults: {
-                email: email,
-                name: name,
-                image: image,
-                id: id,
-            }
-        }
-        )
-        res.status(200).send(userExist);
-    }
-    catch (err) {
-        console.log(err)
+        const userExist = await User.findOne({
+            where: { email: email },
+        });
+        if (!userExist) res.status(404).send({ message: 'User not found' });
+
+        const validPassword = bcrypt.compareSync(
+            password,
+            userExist.password[0]
+        );
+        if (!validPassword) res.status(403).send({ message: 'Wrong Password' });
+
+        const token = await generateAccessToken(userExist.id);
+
+        res.status(200).send({
+            message: 'Login successful',
+            token,
+        });
+    } catch (err) {
+        res.status(500).send({ message: err.message });
     }
 });
 
