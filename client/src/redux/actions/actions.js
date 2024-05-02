f3b8724ba82d57de05d68cde553ce9b7c4b58e0b
t01nk <lautaro.d.gutierrez@gmail.com>
2024-05-02 13:33:07 -0300

    actions rework

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,86 +1,86 @@
-import axios from "axios";
+import axios from 'axios';
 import {
-  FETCH_PRODUCTS,
-  FETCH_CATEGORIES,
-  FETCH_ADMIN_USER,
-  USER_SESSION,
-  ADMIN_SESSION,
-  GET_FAVORITES,
-  CHANGE_COUNT_PRODUCT,
-  FETCH_MOST_SOLD,
-  FETCH_RATING
-} from "./actionTypes";
+    FETCH_PRODUCTS,
+    FETCH_CATEGORIES,
+    FETCH_ADMIN_USER,
+    USER_SESSION,
+    ADMIN_SESSION,
+    GET_FAVORITES,
+    CHANGE_COUNT_PRODUCT,
+    FETCH_MOST_SOLD,
+    FETCH_RATING,
+} from './actionTypes';
 
 export const fetchProducts = async (dispatch) => {
-  const fetchedProducts = await axios.get(
-    `${process.env.REACT_APP_DOMAIN}/product/`
-  );
-  dispatch({
-    type: FETCH_PRODUCTS,
-    payload: fetchedProducts.data,
-  });
+    const fetchedProducts = await axios.get(
+        `${process.env.REACT_APP_DOMAIN}/product/`
+    );
+    dispatch({
+        type: FETCH_PRODUCTS,
+        payload: fetchedProducts.data,
+    });
 };
 
 export const fetchRating = async (dispatch) => {
-  const fetchedrating = await axios.get(
-    `${process.env.REACT_APP_DOMAIN}/product/recommendation/byRating`
-  );
-  
-  dispatch({
-    type: FETCH_RATING,
-    payload: fetchedrating.data,
-  });
+    const fetchedrating = await axios.get(
+        `${process.env.REACT_APP_DOMAIN}/product/recommendation/byRating`
+    );
+
+    dispatch({
+        type: FETCH_RATING,
+        payload: fetchedrating.data,
+    });
 };
 
 export const fetchMostSold = async (dispatch) => {
-  const fetchedMostSold = await axios.get(
-    `${process.env.REACT_APP_DOMAIN}/product/recommendation/mostSold`
-  );
-  
-  dispatch({
-    type: FETCH_MOST_SOLD,
-    payload: fetchedMostSold.data,
-  });
+    const fetchedMostSold = await axios.get(
+        `${process.env.REACT_APP_DOMAIN}/product/recommendation/mostSold`
+    );
+
+    dispatch({
+        type: FETCH_MOST_SOLD,
+        payload: fetchedMostSold.data,
+    });
 };
 
 export const fetchUsers = async (dispatch) => {
-  const fetchedUsers = await axios.get(
-    `${process.env.REACT_APP_DOMAIN}/admin/users`
-  );
-  dispatch({
-    type: FETCH_ADMIN_USER,
-    payload: fetchedUsers.data,
-  });
+    const fetchedUsers = await axios.get(
+        `${process.env.REACT_APP_DOMAIN}/admin/users`
+    );
+    dispatch({
+        type: FETCH_ADMIN_USER,
+        payload: fetchedUsers.data,
+    });
 };
 export const fetchCategories = async (dispatch) => {
-  const fetchedProducts = await axios.get(
-    `${process.env.REACT_APP_DOMAIN}/categories/`
-  );
-  dispatch({
-    type: FETCH_CATEGORIES,
-    payload: fetchedProducts.data,
-  });
+    const fetchedProducts = await axios.get(
+        `${process.env.REACT_APP_DOMAIN}/categories/`
+    );
+    dispatch({
+        type: FETCH_CATEGORIES,
+        payload: fetchedProducts.data,
+    });
 };
 export const checkSession = (dispatch) => {
-  let logged = false;
-  let loggedUser = JSON.parse(localStorage.getItem("myUser"));
-  if (loggedUser) {
-    logged = true;
-    dispatch({
-      type: USER_SESSION,
-      payload: {
-        data: loggedUser,
-        session: logged,
-      },
-    });
-  } else {
-    dispatch({
-      type: USER_SESSION,
-      payload: {
-        session: logged,
-      },
-    });
-  }
+    let logged = false;
+    let loggedUser = JSON.parse(localStorage.getItem('myUser'));
+    if (loggedUser) {
+        logged = true;
+        dispatch({
+            type: USER_SESSION,
+            payload: {
+                data: loggedUser,
+                session: logged,
+            },
+        });
+    } else {
+        dispatch({
+            type: USER_SESSION,
+            payload: {
+                session: logged,
+            },
+        });
+    }
 };
 // export const checkSessionADMIN = async (dispatch) => {
 //   let loggedAdmin = false;
@@ -106,38 +106,38 @@ export const checkSession = (dispatch) => {
 //   }
 // };
 export const getFavorites = async (dispatch, id) => {
-  if (!id) {
-    dispatch({
-      type: GET_FAVORITES,
-      payload: [],
-    });
-  } else {
-    const giveMeFavorites = await axios(
-      `${process.env.REACT_APP_DOMAIN}/user/favorite/${id}`
-    );
-    try {
-      dispatch({
-        type: GET_FAVORITES,
-        payload: giveMeFavorites.data,
-      });
-    } catch (error) {
-      console.log(error);
+    if (!id) {
+        dispatch({
+            type: GET_FAVORITES,
+            payload: [],
+        });
+    } else {
+        const giveMeFavorites = await axios(
+            `${process.env.REACT_APP_DOMAIN}/user/favorite/${id}`
+        );
+        try {
+            dispatch({
+                type: GET_FAVORITES,
+                payload: giveMeFavorites.data,
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }
-  }
 };
 
 export const totalCount = (dispatch) => {
-  let user = JSON.parse(localStorage.getItem("myUser"));
-  let local = JSON.parse(localStorage.getItem(user));
-  if (local) {
-    dispatch({
-      type: CHANGE_COUNT_PRODUCT,
-      payload: local.length,
-    });
-  } else {
-    dispatch({
-      type: CHANGE_COUNT_PRODUCT,
-      payload: 0,
-    });
-  }
+    let user = JSON.parse(localStorage.getItem('myUser'));
+    let local = JSON.parse(localStorage.getItem(user));
+    if (local) {
+        dispatch({
+            type: CHANGE_COUNT_PRODUCT,
+            payload: local.length,
+        });
+    } else {
+        dispatch({
+            type: CHANGE_COUNT_PRODUCT,
+            payload: 0,
+        });
+    }
 };
